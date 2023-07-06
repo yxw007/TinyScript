3eea18b5a5dce423441f9d16ef9461d99a401a29
Potter <75838259@qq.com>
2023-07-06 14:31:54 +0800

    refactor Lexer

---
+++ src/lexer/LexicalException.js
@@ -0,0 +1,11 @@
+class LexicalException extends Error {
+	constructor(msg) {
+		super(msg);
+	}
+
+	static fromChar(c) {
+		return new LexicalException(`unexpected char ${c}`);
+	}
+}
+
+module.exports = LexicalException;
